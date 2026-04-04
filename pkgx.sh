4f5cd3ccf7e6ec572a416c1c29f57d52bcf8677e
Nixie <85381132+nixarchie@users.noreply.github.com>
2026-04-04 21:59:13 +0530

    defined paths

--- pkgx.sh
+++ pkgx.sh
@@ -1,139 +1,139 @@
-#!/usr/bin/env bash
+# #!/usr/bin/env bash
 
-### ---------------- CONFIG ----------------
+# ### ---------------- CONFIG ----------------
 
-# defaults
-: "${PKGX_INTERFACE:=generic}"
-: "${PKGX_ASSUME_YES:=0}"
-: "${PKGX_DRY_RUN:=0}"
-: "${PKGX_DEBUG:=0}"
+# # defaults
+# : "${PKGX_INTERFACE:=generic}"
+# : "${PKGX_ASSUME_YES:=0}"
+# : "${PKGX_DRY_RUN:=0}"
+# : "${PKGX_DEBUG:=0}"
 
-# load configs
-[ -f /etc/pkgx.conf ] && source /etc/pkgx.conf
-[ -f "$HOME/.config/pkgx.conf" ] && source "$HOME/.config/pkgx.conf"
+# # load configs
+# [ -f /etc/pkgx.conf ] && source /etc/pkgx.conf
+# [ -f "$HOME/.config/pkgx.conf" ] && source "$HOME/.config/pkgx.conf"
 
-[ "$PKGX_DEBUG" = 1 ] && set -x
+# [ "$PKGX_DEBUG" = 1 ] && set -x
 
-### ---------------- FLAGS PARSER ----------------
+# ### ---------------- FLAGS PARSER ----------------
 
-ARGS=()
+# ARGS=()
 
-for arg in "$@"; do
-  case "$arg" in
-    -y|--yes) PKGX_ASSUME_YES=1 ;;
-    --dry-run) PKGX_DRY_RUN=1 ;;
-    --debug) PKGX_DEBUG=1 ;;
-    --interface=*) PKGX_INTERFACE="${arg#*=}" ;;
-    -h|--help)
-      echo "Usage: pkgx <command> [packages] [flags]"
-      exit 0
-      ;;
-    *) ARGS+=("$arg") ;;
-  esac
-done
+# for arg in "$@"; do
+#   case "$arg" in
+#     -y|--yes) PKGX_ASSUME_YES=1 ;;
+#     --dry-run) PKGX_DRY_RUN=1 ;;
+#     --debug) PKGX_DEBUG=1 ;;
+#     --interface=*) PKGX_INTERFACE="${arg#*=}" ;;
+#     -h|--help)
+#       echo "Usage: pkgx <command> [packages] [flags]"
+#       exit 0
+#       ;;
+#     *) ARGS+=("$arg") ;;
+#   esac
+# done
 
-# restore positional args
-set -- "${ARGS[@]}"
+# # restore positional args
+# set -- "${ARGS[@]}"
 
-### ---------------- COMMAND MAP ----------------
+# ### ---------------- COMMAND MAP ----------------
 
-declare -A CMD_MAP
+# declare -A CMD_MAP
 
-load_interface() {
-  case "$PKGX_INTERFACE" in
-    pacman)
-      CMD_MAP=(
-        [-S]=install_deps
-        [-R]=remove_deps
-        [-Ss]=search_pkg
-        [-Sy]=sync_db
-      )
-      ;;
-    apt)
-      CMD_MAP=(
-        [install]=install_deps
-        [remove]=remove_deps
-        [search]=search_pkg
-        [update]=sync_db
-      )
-      ;;
-    generic|*)
-      CMD_MAP=(
-        [install]=install_deps
-        [add]=install_deps
-        [i]=install_deps
+# load_interface() {
+#   case "$PKGX_INTERFACE" in
+#     pacman)
+#       CMD_MAP=(
+#         [-S]=install_deps
+#         [-R]=remove_deps
+#         [-Ss]=search_pkg
+#         [-Sy]=sync_db
+#       )
+#       ;;
+#     apt)
+#       CMD_MAP=(
+#         [install]=install_deps
+#         [remove]=remove_deps
+#         [search]=search_pkg
+#         [update]=sync_db
+#       )
+#       ;;
+#     generic|*)
+#       CMD_MAP=(
+#         [install]=install_deps
+#         [add]=install_deps
+#         [i]=install_deps
 
-        [remove]=remove_deps
-        [rm]=remove_deps
+#         [remove]=remove_deps
+#         [rm]=remove_deps
 
-        [search]=search_pkg
-        [sync]=sync_db
-      )
-      ;;
-  esac
-}
+#         [search]=search_pkg
+#         [sync]=sync_db
+#       )
+#       ;;
+#   esac
+# }
 
-load_interface
+# load_interface
 
-### ---------------- COMMAND RESOLUTION ----------------
+# ### ---------------- COMMAND RESOLUTION ----------------
 
-CMD="$1"
-shift
+# CMD="$1"
+# shift
 
-FUNC="${CMD_MAP[$CMD]}"
+# FUNC="${CMD_MAP[$CMD]}"
 
-if ! declare -f "$FUNC" >/dev/null; then
-  echo "Unknown command: $CMD"
-  exit 1
-fi
+# if ! declare -f "$FUNC" >/dev/null; then
+#   echo "Unknown command: $CMD"
+#   exit 1
+# fi
 
-### ---------------- UTIL ----------------
+# ### ---------------- UTIL ----------------
 
-run_cmd() {
-  if [ "$PKGX_DRY_RUN" = 1 ]; then
-    echo "[DRY RUN] $*"
-  else
-    "$@"
-  fi
-}
+# run_cmd() {
+#   if [ "$PKGX_DRY_RUN" = 1 ]; then
+#     echo "[DRY RUN] $*"
+#   else
+#     "$@"
+#   fi
+# }
 
-### ---------------- CORE FUNCTIONS ----------------
+# ### ---------------- CORE FUNCTIONS ----------------
 
-install_deps() {
-  if command -v pacman >/dev/null; then
-    [ "$PKGX_ASSUME_YES" = 1 ] && YES="--noconfirm"
-    run_cmd pacman -S $YES "$@"
-  elif command -v apt >/dev/null; then
-    run_cmd apt install -y "$@"
-  else
-    echo "No supported backend found"
-  fi
-}
+# install_deps() {
+#   if command -v pacman >/dev/null; then
+#     [ "$PKGX_ASSUME_YES" = 1 ] && YES="--noconfirm"
+#     run_cmd pacman -S $YES "$@"
+#   elif command -v apt >/dev/null; then
+#     run_cmd apt install -y "$@"
+#   else
+#     echo "No supported backend found"
+#   fi
+# }
 
-remove_deps() {
-  if command -v pacman >/dev/null; then
-    run_cmd pacman -R "$@"
-  elif command -v apt >/dev/null; then
-    run_cmd apt remove "$@"
-  fi
-}
+# remove_deps() {
+#   if command -v pacman >/dev/null; then
+#     run_cmd pacman -R "$@"
+#   elif command -v apt >/dev/null; then
+#     run_cmd apt remove "$@"
+#   fi
+# }
 
-search_pkg() {
-  if command -v pacman >/dev/null; then
-    run_cmd pacman -Ss "$@"
-  elif command -v apt >/dev/null; then
-    run_cmd apt search "$@"
-  fi
-}
+# search_pkg() {
+#   if command -v pacman >/dev/null; then
+#     run_cmd pacman -Ss "$@"
+#   elif command -v apt >/dev/null; then
+#     run_cmd apt search "$@"
+#   fi
+# }
 
-sync_db() {
-  if command -v pacman >/dev/null; then
-    run_cmd pacman -Sy
-  elif command -v apt >/dev/null; then
-    run_cmd apt update
-  fi
-}
+# sync_db() {
+#   if command -v pacman >/dev/null; then
+#     run_cmd pacman -Sy
+#   elif command -v apt >/dev/null; then
+#     run_cmd apt update
+#   fi
+# }
 
-### ---------------- EXECUTE ----------------
+# ### ---------------- EXECUTE ----------------
 
-"$FUNC" "$@"
+# "$FUNC" "$@"
